ce280eac5ec307f14a80ea5d018d54d0f480961a
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-22 00:20:17 +0530

    Add files via upload

---
+++ Operator/Assignment_operator.java
@@ -0,0 +1,19 @@
+
+public class Assignment_operator {
+    public static void main(String[] args) {
+
+        int a = 10;
+        a += 5; // a= a+5
+        int b = 15;
+        b -= 5;// b= b-5
+        int c = 4;
+        c *= 5; // c= c*5
+        int d = 50;
+        d /= 5; // d= d/5
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+
+}
