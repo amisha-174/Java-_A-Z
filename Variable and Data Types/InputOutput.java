350e8810bdc4cd06fc267894bf57a967defbb3a4
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-21 13:59:55 +0530

    Add files via upload

---
+++ Variable and Data Types/InputOutput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class InputOutput {
+
+    public static void main(String[] args) {
+
+        // input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        // output
+        System.out.println(a);
+
+    }
+}
