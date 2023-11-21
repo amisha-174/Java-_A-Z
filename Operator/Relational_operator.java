ff2e237311e55bba2b4ad3439710606f3d2850af
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-22 00:19:51 +0530

    Add files via upload

---
+++ Operator/Relational_operator.java
@@ -0,0 +1,16 @@
+
+public class Relational_operator {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        System.out.println(a == b);// equal (False)
+        System.out.println(a != b); // not equal (True)
+        System.out.println(a > b); // greater than(False)
+        System.out.println(a < b); // less than(True)
+        System.out.println(a >= b); // greater than or equal to(False)
+        System.out.println(a <= b); // less than or equal to(True)
+
+    }
+
+}
