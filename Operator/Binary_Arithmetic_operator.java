2ceb23ab039b5ef6cb7508209003a1b8a9ef1885
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-22 00:18:22 +0530

    Add files via upload

---
+++ Operator/Binary_Arithmetic_operator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Binary_Arithmetic_operator {
+
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 4;
+        // Arithmetic operator are +(Addition), -(Substraction), *(Multiplication),
+        // /(Division)
+        System.out.println("addition =" + (a + b));
+        System.out.println("Substraction =" + (a - b));
+        System.out.println("Multiplication =" + (a * b));
+        System.out.println("division =" + (a / b));
+        System.out.println("remainder =" + (a % b));
+
+    }
+
+}
