c3f8594c84d390ee52cc2ae6e7dca3d7a9937477
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-22 00:19:01 +0530

    Add files via upload

---
+++ Operator/logical_operator.java
@@ -0,0 +1,11 @@
+
+public class logical_operator {
+    public static void main(String[] args) {
+        // logical operator are &&(logical AND) , ||(logical OR), !(logical NOT )
+
+        System.out.println((3 > 2) && (6 > 5));
+        System.out.println((3 < 2) || (6 > 5));
+        System.out.println(!(3 > 2));
+    }
+
+}
