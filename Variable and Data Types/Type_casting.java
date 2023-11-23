576c33c6988a426a75b606436adb5794948a21d8
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-23 23:00:44 +0530

    Add files via upload

---
+++ Variable and Data Types/Type_casting.java
@@ -0,0 +1,14 @@
+public class Type_casting {
+
+    public static void main(String[] args) {
+        float a = 30.55f;
+        int b = (int) a;
+        System.out.println(b);
+
+        char ch = 'a';
+        int num = ch;
+        System.out.println(num);
+
+    }
+
+}
