7af5963d0b2c4beb57e099a8b0ef6e0818acb2f4
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-23 23:06:57 +0530

    Add files via upload

---
+++ Conditional_statement/switch_case.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class switch_case {
+
+    public static void main(String[] args) {
+        int num = 2;
+        switch (num) {
+            case 1:
+                System.out.println("hi");
+                break;
+            case 2:
+                System.out.println("Hello");
+                break;
+            case 3:
+                System.out.println("Namaste");
+                break;
+            default:
+                System.out.println("Wrong input");
+
+        }
+
+    }
+}
