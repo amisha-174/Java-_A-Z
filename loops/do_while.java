c8cd5db6bc9102df203e1d119c0a58a98eb01e10
amisha-174 <110594730+amisha-174@users.noreply.github.com>
2023-11-24 21:29:13 +0530

    Add files via upload

---
+++ loops/do_while.java
@@ -0,0 +1,12 @@
+
+public class do_while {
+    public static void main(String[] args) {
+        int n = 1;
+        do {
+            System.out.println("hello world");
+            n++;
+
+        } while (n <= 5);
+    }
+
+}
